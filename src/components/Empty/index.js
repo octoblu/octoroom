00fce5e9387cb75589c2e495a88f7ca7e98d0699
Sqrt of Saturn <sqrt@octoblu.com>
2016-08-30 12:53:03 -0700

    added qr to welcome

--- src/components/Empty/index.js
+++ src/components/Empty/index.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import View from 'react-flexbox'
+import QrCode from '../QrCode/'
 
 import styles from './styles.css'
 
@@ -10,6 +11,9 @@ const Empty = () => {
   return (
     <View auto column className={styles.welcome}>
       <div>Welcome to the OctoRoom</div>
+
+      <QrCode url="www.google.com" />
+
     </View>
   )
 }
