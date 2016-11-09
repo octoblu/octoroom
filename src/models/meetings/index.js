dc4433f58d6e1aad7d3dc10eb0d257090c3a4388
Roy van de Water <r@octoblu.com>
2016-11-08 18:11:01 -0700

    v2.0.10 Add debug

--- src/models/meetings/index.js
+++ src/models/meetings/index.js
@@ -1,13 +1,24 @@
+import debug from 'debug'
 import _ from 'lodash'
 import moment from 'moment'
+const log = debug('models:meetings')
 
 const getNextMeeting = (meetings) => {
-  const futureMeetings =  _.filter(meetings, ({startTime, ended}) => {
+  const futureMeetings =  _.filter(meetings, ({subject, startTime, ended}) => {
     const now = moment()
 
-    if (ended) return false
-    if (moment(startTime).isBefore(now)) return false
-    if (!now.isSame(startTime, 'day')) return false
+    if (ended) {
+      log(subject, 'not a future meeting cause it ended')
+      return false
+    }
+    if (moment(startTime).isBefore(now)) {
+      log(subject, 'not a future meeting cause it started in the past')
+      return false
+    }
+    if (!now.isSame(startTime, 'day')) {
+      log(subject, 'not a future meeting cause it aint in today')
+      return false
+    }
     return true
   })
 
