4b09ecc28310f889d4e313152ad8bd7e651dd418
Roy van de Water <r@octoblu.com>
2016-12-05 12:04:46 -0700

    v5.0.2 Use new firehose stack

--- src/config/development.js
+++ src/config/development.js
@@ -3,6 +3,6 @@ export const OAUTH_CLIENT_ID = '0507cf42-095b-49e5-831b-7ce78aa48480'
 export const OAUTH_PROVIDER_URI = 'https://oauth.octoblu.com'
 export const FIREHOSE_CONFIG = {
   protocol: 'https',
-  hostname: 'meshblu-firehose-socket-io.octoblu.com',
+  hostname: 'meshblu-firehose-socket-io.smartspaces.octoblu.com',
   port: 443,
 }
