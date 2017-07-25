243215676c39fcc698b9328d519745dee944bfb0
kmariano / oayandosu <koshin.mariano@gmail.com , oayandosu@gmail.com>
2017-07-24 17:11:58 -0700

    v8.3.0 Change early meeting prompt

--- src/components/StartMeetingEarlyPrompt/index.js
+++ src/components/StartMeetingEarlyPrompt/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import StateSubHeading from "../StateSubHeading"
 
 const StartMeetingEarlyPrompt = () => (
-  <StateSubHeading>Press the button to start the meeting early</StateSubHeading>
+  <StateSubHeading>Press the button to start the call early</StateSubHeading>
 )
 
 export default StartMeetingEarlyPrompt
